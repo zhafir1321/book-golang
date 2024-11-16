34532dbf787ee140d62eec4e6cee55914c1df917
Zhafir <zahfir1000@hotmail.com>
2024-11-16 21:38:06 +0700

    fixed: middlewares for update book and delete book

--- routes/book.go
+++ routes/book.go
@@ -15,7 +15,7 @@ func BookRoutes(r *mux.Router) {
 	router.HandleFunc("/", controllers.GetBooks).Methods("GET")
 	router.HandleFunc("/{id}", controllers.GetBook).Methods("GET")
 	router.Handle("/", middlewares.Authorization(http.HandlerFunc(controllers.CreateBook))).Methods("POST")
-	router.HandleFunc("/{id}", controllers.UpdateBook).Methods("PUT")
+	router.Handle("/{id}", middlewares.Authorization(http.HandlerFunc(controllers.UpdateBook))).Methods("PUT")
 	router.Handle("/{id}", middlewares.Authorization(http.HandlerFunc(controllers.DeleteBook))).Methods("DELETE")
 
 }
